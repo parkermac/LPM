dbdf8bed4ba5ca3cd98077be4b274ad9148b99ba
Parker MacCready <p.maccready@gmail.com>
2021-11-30 16:18:36 -0800

    hypoxic volume

---
+++ extract/tef/extract_hvol_segment_one_time.py
@@ -0,0 +1,100 @@
+"""
+This code extracts all needed hypoxic volume segment data for one history file,
+looping over all segments.
+
+"""
+from pathlib import Path
+import sys
+import argparse
+import numpy as np
+import xarray as xr
+from time import time
+import pickle
+import pandas as pd
+
+from lo_tools import Lfun, zfun, zrfun
+
+# command line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('-pth', type=str) # path to LO_roms
+parser.add_argument('-out_dir', type=str) # path to temporary directory for output
+parser.add_argument('-gtagex', type=str) # gtagex
+parser.add_argument('-d', type=str) # date string like 2019.07.04
+parser.add_argument('-nhis', type=int, default=1) # history file number, 1-25
+parser.add_argument('-testing', type=Lfun.boolean_string, default=False)
+args = parser.parse_args()
+
+Ldir = Lfun.Lstart(gridname=args.gtagex.split('_')[0])
+
+pth = Ldir['LO'] / 'extract' / 'tef'
+if str(pth) not in sys.path:
+    sys.path.append(str(pth))
+import tef_fun
+
+nhiss = ('0000' + str(args.nhis))[-4:]
+fn = Path(args.pth) / args.gtagex / ('f' + args.d) / ('ocean_his_' + nhiss + '.nc')
+out_dir = Path(args.out_dir)
+Lfun.make_dir(out_dir)
+out_fn  = out_dir / ('A_' + args.d + '_' + nhiss + '.p')
+out_fn.unlink(missing_ok=True)
+
+# ---
+# get grid info
+G, S, T = zrfun.get_basic_info(fn)
+h = G['h']
+DA = G['DX'] * G['DY']
+DA3 = DA.reshape((1,G['M'],G['L']))
+
+# get segment info
+vol_dir = Ldir['LOo'] / 'extract' / 'tef' / ('volumes_' + Ldir['gridname'])
+v_df = pd.read_pickle(vol_dir / 'volumes.p')
+j_dict = pickle.load(open(vol_dir / 'j_dict.p', 'rb'))
+i_dict = pickle.load(open(vol_dir / 'i_dict.p', 'rb'))
+seg_list = list(v_df.index)
+
+# set list of variables to extract
+vn_list = ['oxygen']
+
+tt0 = time()
+print(fn)
+    
+ds = xr.open_dataset(fn)
+
+vn_dict = {}
+for vn in vn_list:
+    vn_dict[vn] = ds[vn][0,:,:,:].values
+zeta = ds['zeta'][0,:,:].values
+ds.close()
+
+# find the hypoxic volume and other variables for each segment, at this time
+A = pd.DataFrame(index=seg_list)
+AA = dict()
+for seg_name in seg_list:
+    
+    jjj = j_dict[seg_name]
+    iii = i_dict[seg_name]
+    z_w = zrfun.get_z(h[jjj,iii], zeta[jjj,iii], S, only_w=True)
+    dz = np.diff(z_w, axis=0)
+    DV = dz * DA3[0,jjj,iii]
+    volume = DV.sum()
+    AA['volume'] = volume
+    
+    mask = vn_dict['oxygen'][:,jjj,iii] > 60
+    DV_hvol = DV.copy()
+    DV_hvol[mask] = 0
+    AA['hypoxic_volume'] = DV_hvol.sum()
+    
+    for vn in vn_list:
+        AA[vn] = (vn_dict[vn][:,jjj,iii] * DV).sum()/volume
+            
+    # store results
+    for vn in vn_list + ['volume', 'hypoxic_volume']:
+        A.loc[seg_name, vn] = AA[vn]
+            
+print('  ** took %0.1f sec' % (time()-tt0))
+sys.stdout.flush()
+
+A.to_pickle(out_fn)
+print('Time to extract all segment data = %0.2f sec' % (time()-tt0))
+sys.stdout.flush()
+
---
+++ extract/tef/extract_hvol_segments.py
@@ -0,0 +1,133 @@
+"""
+A tool to extract time series of hypoxic volume in the segments.
+
+To test on mac:
+run extract_hvol_segments -gtx cas6_v3_lo8b -ro 2 -0 2019.07.04 -1 2019.07.06
+
+Performance:
+"""
+from lo_tools import Lfun, zrfun
+from lo_tools import extract_argfun as exfun
+Ldir = exfun.intro() # this handles the argument passing
+
+import sys
+from time import time
+import numpy as np
+import netCDF4 as nc
+import pickle
+import pandas as pd
+from subprocess import Popen as Po
+from subprocess import PIPE as Pi
+import xarray as xr
+
+pth = Ldir['LO'] / 'extract' / 'tef'
+if str(pth) not in sys.path:
+    sys.path.append(str(pth))
+import tef_fun
+
+ds0 = Ldir['ds0']
+ds1 = Ldir['ds1']
+
+tt00 = time()
+print(' Doing hypoxic volume extraction for '.center(60,'='))
+print(' gtagex = ' + Ldir['gtagex'])
+outname = 'hvol_segments_' + ds0 + '_' + ds1 + '.nc'
+print(' outname = ' + outname)
+# make sure the output directory exists
+out_dir = Ldir['LOo'] / 'extract' / Ldir['gtagex'] / 'tef'
+Lfun.make_dir(out_dir)
+# name the output file and make sure it does not exist
+out_fn = out_dir / outname
+out_fn.unlink(missing_ok=True)
+
+# make the scratch directory for holding temporary files
+temp_dir = Ldir['LOo'] / 'extract' / Ldir['gtagex'] / ('hvol_segment_temp_' + ds0 + '_' + ds1)
+Lfun.make_dir(temp_dir, clean=True)
+
+# get list of history files to process
+fn_list = Lfun.get_fn_list('daily', Ldir, ds0, ds1)
+    
+print('Doing initial data extraction:')
+# We do extractions one file at a time, as separate subprocess jobs.
+# Files are saved to temp_dir.
+tt000 = time()
+proc_list = []
+N = len(fn_list)
+for ii in range(N):
+    fn = fn_list[ii]
+    d = fn.parent.name.replace('f','')
+    nhis = int(fn.name.split('.')[0].split('_')[-1])
+    cmd_list = ['python3', 'extract_hvol_segment_one_time.py',
+            '-pth', str(Ldir['roms_out']),
+            '-out_dir',str(temp_dir),
+            '-gtagex', Ldir['gtagex'],
+            '-d', d, '-nhis', str(nhis),
+            '-test', str(Ldir['testing'])]
+    proc = Po(cmd_list, stdout=Pi, stderr=Pi)
+    proc_list.append(proc)
+    
+    Nproc = Ldir['Nproc']
+    if ((np.mod(ii,Nproc) == 0) and (ii > 0)) or (ii == N-1):
+        tt0 = time()
+        for proc in proc_list:
+            proc.communicate()
+        print(' - %d out of %d: %d took %0.2f sec' % (ii, N, Nproc, time()-tt0))
+        sys.stdout.flush()
+        proc_list = []
+print('Total elapsed time = %0.2f sec' % (time()-tt000))
+
+"""
+Now we repackage all these single-time extractions into an xarray Dataset.  This is
+a little tricky because for the Dataset we want to have it composed of DataArrays that
+are variable(time, segment), but what we are starting from is a collection of
+time(segment, variable) pandas DataFrames.
+
+We will proceed by using a two step process, first concatenating all the pandas DataFrames
+into an xarray DataArray with dimensions(time, segment, variable).  Then we will
+convert this to an xarray Dataset with a collection of variable(time,segment) DataArrays.
+
+I'm sure there is a more clever way to do this in xarray, but I am not yet proficient
+enough with that module.
+"""
+
+# get a list of all our pandas DataFrames
+A_list = list(temp_dir.glob('A*.p'))
+A_list.sort()
+# make a list of the datetimes and form a time index
+ot_list = []
+for fn in fn_list:
+    ds = xr.open_dataset(fn)
+    ot = ds['ocean_time'].values[0]
+    ot_list.append(ot)
+ot_ind = pd.Index(ot_list)
+# make a list of the output as DataArrays
+x_list = []
+for A_fn in A_list:
+    A = pd.read_pickle(A_fn)
+    x_list.append(xr.DataArray(A, dims=('seg','vn')))
+# and concatenate that list into a single DataArray, with time as the concatenating dimension
+da = xr.concat(x_list, pd.Index(ot_ind, name='time'))
+# repackage the DataArray as a Dataset
+vns = da.coords['vn'].values
+segs = da.coords['seg'].values
+times = da.coords['time'].values
+ds = xr.Dataset(coords={'time': times,'seg': segs})
+for vn in vns:
+    v = da.sel(vn=vn).values
+    ds[vn] = (('time','seg'), v)
+# save it to NetCDF
+ds.to_netcdf(out_fn)
+ds.close()
+
+# Clean up
+if not Ldir['testing']:
+    Lfun.make_dir(temp_dir, clean=True)
+    temp_dir.rmdir()
+
+if Ldir['testing']:
+    # check results
+    dd = xr.open_dataset(out_fn)
+    print(dd.hypoxic_volume.sel(seg='J1').values)
+    dd.close()
+
+
