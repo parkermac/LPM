122f8fff99d0800f882b3074226fdd662516bc64
Parker MacCready <p.maccready@gmail.com>
2022-05-29 10:50:04 -0700

    Update salt_budget.py
    cleaning up

--- extract/tef/salt_budget.py
+++ extract/tef/salt_budget.py
@@ -17,13 +17,14 @@ import pickle
 import pandas as pd
 import argparse
 import xarray as xr
+from datetime import datetime, timedelta
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-g', '--gridname', type=str, default='cas6')
 parser.add_argument('-t', '--tag', type=str, default='v3')
 parser.add_argument('-x', '--ex_name', type=str, default='lo8b')
 parser.add_argument('-yr', '--year', type=int, default=2018)
-parser.add_argument('-test', '--testing', type=zfun.boolean_string, default=True)
+parser.add_argument('-test', '--testing', type=zfun.boolean_string, default=False)
 args = parser.parse_args()
 testing = args.testing
 year = args.year
@@ -32,23 +33,21 @@ year = args.year
 Ldir = Lfun.Lstart(gridname=args.gridname, tag=args.tag, ex_name=args.ex_name)
 
 # more imports
-sys.path.append(str(Ldir['LO'] / 'extract' /'tef'))
+pth = str(Ldir['LO'] / 'extract' /'tef')
+if pth not in sys.path:
+    sys.path.append(pth)
 import tef_fun
 import flux_fun
 
-if testing:
-    vol_list = ['Puget Sound']
-else:
-    vol_list = ['Salish Sea', 'Puget Sound', 'Hood Canal']
+vol_list = ['South Sound', 'Puget Sound']
+#vol_list = ['Salish Sea', 'Puget Sound', 'Hood Canal', 'South Sound']
+
+plt.close('all')
 
 for which_vol in vol_list:
 
     year_str = str(year)
     date_str = '_' + year_str + '.01.01_' + year_str + '.12.31'
-
-    # out_dir = (Ldir['LOo'] / 'extract' / Ldir['gtagex'] / 'tef' /
-    #     ('Budgets_' + which_vol.replace(' ','_') + '_' + year_str))
-    # Lfun.make_dir(out_dir, clean=True)
     
     # get paths to all required data [For cas6_v3_lo8b I have 2017-2020]
     riv_fn = Ldir['LOo'] / 'pre' / 'river' / Ldir['gtag'] / 'Data_roms' / ('extraction' + date_str + '.nc')
@@ -70,13 +69,16 @@ for which_vol in vol_list:
     elif which_vol == 'Hood Canal':
         seg_list = flux_fun.ssH
         sect_sign_dict = {'hc1':-1}
+    elif which_vol == 'South Sound':
+        seg_list = flux_fun.ssT + flux_fun.ssS
+        sect_sign_dict = {'tn1':-1}
 
     # SECTION INFO
     sect_df = tef_fun.get_sect_df(Ldir['gridname'])
 
     # RIVERS
     """
-    These are now stored in an xr.Dataset:
+    These are stored in an xr.Dataset:
     time = daily, noon of each day
     riv = river names
     variable names: transport + all the tracers in tef_fun.vn_list
@@ -98,7 +100,7 @@ for which_vol in vol_list:
             
     # SEGMENT TIME SERIES
     """
-    These are now stored in an xr.Dataset:
+    These are stored in an xr.Dataset:
     time = hourly (so we lowpass, subsample, and clip the ends)
     seg = segment names
     variable names = volume + all the tracers in tef_fun.vn_list
@@ -124,17 +126,12 @@ for which_vol in vol_list:
         
     # rate of change of volume-integrated tracer (sum(C*v)/sec)
     cvt_lp_dict = {}
-    for vn in ['salt']:#tef_fun.vn_list:
-        cvt = nanvec.copy()
-        cvt[1:-1] = (seg_ds.volume[2:].values*seg_ds[vn][2:].values
-            - seg_ds.volume[:-2].values*seg_ds[vn][:-2].values).sum(axis=1)/(2*3600)
-        cvt_lp_dict[vn] = zfun.lowpass(cvt, f='godin')[pad:-pad+1:24]
-        
-    # volume- and time-averaged tracer
-    vmean_dict = dict()
-    for vn in ['salt']:#tef_fun.vn_list:
-        vmean_dict[vn] = ((seg_ds.volume*seg_ds[vn]).sum(axis=1)/V).mean().values
-        
+    vn = 'salt'
+    cvt = nanvec.copy()
+    cvt[1:-1] = (seg_ds.volume[2:].values*seg_ds[vn][2:].values
+        - seg_ds.volume[:-2].values*seg_ds[vn][:-2].values).sum(axis=1)/(2*3600)
+    cvt_lp = zfun.lowpass(cvt, f='godin')[pad:-pad+1:24]
+                
     # BUDGETS
     
     # time index to use
@@ -151,89 +148,76 @@ for which_vol in vol_list:
     vol_df['Error'] = vol_df['dV_dt'] - vol_df.loc[:,'Qin'] - vol_df.loc[:,'Qout'] - vol_df.loc[:,'Qr']
     vol_rel_err = vol_df['Error'].mean()/vol_df['Qr'].mean()
 
-    # Tracer budgets
-    # F is the "flux" of a tracer, with units [tracer units]*m3/s
-    # Ftot, Fin, and Fout are at the ocen boundaries of the volume.  Fout is negative.
-    C = dict()
-    # The "normalized" budgets are averaged over a year, multiplied by a year of seconds,
-    # and divided by the mean volume, so they have units [tracer units].  So Cnorm['NO3']['dFnet_dt']
-    # is the change in total mean Nitrate over the year, and the other terms in Cnorm['NO3'] tell you where
-    # that change came from.  I'm not sure this is the right normalization to use.
-    # Cnorm = dict()
-    for vn in ['salt']:#tef_fun.vn_list:
-        c_df = pd.DataFrame(0, index=indall, columns=['Ftot','Fin','Fout', 'QinDS', 'QrSout'])
-        Qin = 0
-        QSin = 0
-        Qout = 0
-        QSout = 0
-        for sect_name in sect_list:
-            df = tef_df_dict[sect_name]
-            c_df['Ftot'] = c_df['Ftot'] + df['Qin']*df[vn+'_in'] + df['Qout']*df[vn+'_out']
-            c_df['Fin'] = c_df['Fin'] + df['Qin']*df[vn+'_in']
-            c_df['Fout'] = c_df['Fout'] + df['Qout']*df[vn+'_out']
-            
-            # custom salt budget things
-            Qin += df['Qin']
-            QSin += df['Qin']*df['salt_in']
-            Qout += df['Qout']
-            QSout += df['Qout']*df['salt_out']
-            
-        Sin = QSin / Qin
-        Sout = QSout / Qout
-        DS = Sin - Sout
-        Qr_adj = vol_df['Qr'] - vol_df['dV_dt']
-        c_df['QinDS'] = Qin * DS
-        c_df['QrSout'] = Qr_adj * Sout
-            
-        c_df['Fr'] = (riv_ds.transport * riv_ds[vn]).sum(axis=1)[1:-1]
-        c_df.loc[:, 'dFnet_dt'] = cvt_lp_dict[vn]
-        # the residual of the budget is assumed to be an unresolved Source or Sink (Sink is negative)
-        # e.g. due to air-sea gas transfer, denitrification, or internal conversion to another tracer.
-        #c_df['Source/Sink'] = c_df['dFnet_dt'] - c_df['Ftot'] - c_df['Fr']
-        c_df['Source/Sink'] = c_df['dFnet_dt'] - c_df['QinDS'] + c_df['QrSout']
-        C[vn] = c_df.copy()
-        
-        # cn = c_df.mean()*(365*86400)/V
-        # cn = cn.rename({'dFnet_dt':'Change in Concentration', 'Ftot':'Inflow+Outflow',
-        #     'Fin':'Inflow', 'Fout':'Outflow', 'Fr':'River'})
-        # cn = cn[['Change in Concentration', 'Inflow', 'Outflow', 'Inflow+Outflow', 'River','Source/Sink']]
-        # cn['Mean Concentration'] = vmean_dict[vn]
-        # Cnorm[vn] = cn
-        
-    # C['Ntot'] = C['NO3']+C['phytoplankton']+C['zooplankton']+C['detritus']+C['Ldetritus']
-    # Cnorm['Ntot'] = Cnorm['NO3']+Cnorm['phytoplankton']+Cnorm['zooplankton']+Cnorm['detritus']+Cnorm['Ldetritus']
+    # Tracer budget
+    Qin = 0
+    QSin = 0
+    Qout = 0
+    QSout = 0
+    Qprism = 0
+    # sum over all the open sections
+    for sect_name in sect_list:
+        df = tef_df_dict[sect_name]
+        # quantities we use below for derived quantities
+        Qin += df['Qin']
+        QSin += df['Qin']*df['salt_in']
+        Qout += df['Qout']
+        QSout += df['Qout']*df['salt_out']
+        Qprism += df['qabs']/2
+    # derived 
+    Sin = QSin / Qin
+    Sout = QSout / Qout
+    DS = Sin - Sout
+    Qr = vol_df['Qr']
     
-    plt.close('all')
+    # Fill the budget Dataframe
+    c_df = pd.DataFrame(index=indall)
+    c_df['QinDS'] = Qin * DS / 1000
+    c_df['-QrSout'] = - Qr * Sout / 1000
+    # We include the time-varying volume term in the storage term
+    c_df['Storage'] = (cvt_lp - vol_df['dV_dt'] * Sout) / 1000
+    
+    # The residual of the budget is the error (Sink is negative)
+    c_df['Error'] = c_df['Storage'] - c_df['QinDS'] - c_df['-QrSout']
+    
+    # add Qprism and Qr, for plotting
+    c_df['Qprism'] = Qprism /1000
+    c_df['Qr'] = Qr / 1000
+    
+    # Plotting
     pfun.start_plot()
-    
     fig = plt.figure()
-    ax = fig.add_subplot(111)
-    tstr = which_vol + ' Volume Budget [m3/s]'
-    vol_df.plot(ax=ax, grid=True, title=tstr)
-    if testing:
-        plt.show()
-    else:
-        fig.savefig(out_dir / 'volume.png')
-        
-    for vn in C.keys():
-        fig = plt.figure()
-        ax = fig.add_subplot(111)
-        tstr = which_vol + ' ' + vn + ' Budget [g/kg m3/s]'
-        # C[vn][['dFnet_dt','Ftot','Fr','Source/Sink']].plot(ax=ax, grid=True, title=tstr)
-        C[vn][['dFnet_dt','QinDS','QrSout','Source/Sink']].plot(ax=ax, grid=True, title=tstr)
-        if testing:
-            #pass
-            plt.show()
-        else:
-            fig.savefig(out_dir / (vn + '.png'))
-
-    # # text output
-    # with open(out_dir / ('Annual_Mean_' + which_vol.replace(' ','_') + '_' + year_str + '.txt'), 'w') as fout:
-    #     for vn in C.keys():
-    #         tstr = ' ' + which_vol + ' ' + vn + ' Annual Mean [' + units_dict[vn] + '] '
-    #         fout.write(tstr.center(51,'=') + '\n')
-    #         for k in Cnorm[vn].keys():
-    #             fout.write('%25s %25.3f\n' % (k, Cnorm[vn][k]))
-        
+    dt0 = datetime(year,1,1)
+    dt1 = datetime(year,12,31)
+    
+    ax = fig.add_subplot(311)
+    tstr = which_vol + ' Salt Budget [1000 g/kg m3/s]'
+    c_df[['Storage','QinDS','-QrSout','Error']].plot(ax=ax, title=tstr,
+        style={'Storage':'-b', 'QinDS':'-r', '-QrSout':'-g', 'Error':'-c'})
+    ax.set_xticklabels([])
+    ax.set_xlim(dt0, dt1)
+    ax.legend(ncol=4)
+    ax.grid(True)
+    
+    ax = fig.add_subplot(312)
+    ax2 = ax.twinx()
+    c_df[['QinDS']].plot(ax=ax, c='r')
+    c_df[['Qprism']].plot(ax=ax2, c='purple')
+    ax.set_xticklabels([])
+    ax.set_xlim(dt0, dt1)
+    ax.set_ylim(bottom=0)
+    ax2.set_ylim(bottom=0)
+    ax.grid(axis='x')
+    ax2.grid(axis='x')
+    
+    ax = fig.add_subplot(313)
+    ax2 = ax.twinx()
+    c_df[['-QrSout']].plot(ax=ax, c='g')
+    c_df[['Qr']].plot(ax=ax2, style='--g')
+    ax.set_xlim(dt0, dt1)
+    ax2.set_ylim(bottom=0)
+    ax.grid(axis='x')
+    ax2.grid(axis='x')
+    
+    plt.show()
     pfun.end_plot()
 
