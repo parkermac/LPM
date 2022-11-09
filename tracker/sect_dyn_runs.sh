02e25f9c674ed358ca5c506f6c2f82fc62cc0f74
Parker MacCready <p.maccready@gmail.com>
2022-11-09 09:57:38 -0800

    Update sect_dyn_runs.sh
    make all run at the same time

--- tracker/sect_dyn_runs.sh
+++ tracker/sect_dyn_runs.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 rundir='/dat1/parker/LO/tracker'
-python $rundir/tracker.py -gtx cas6_v00_uu0mb -exp sect_AImid -3d True -dtt 2 -clb True -d 2021.07.25 -sh 20 -sub_tag SpringSBF > $rundir/SpringSBF.log
-python $rundir/tracker.py -gtx cas6_v00_uu0mb -exp sect_AImid -3d True -dtt 2 -clb True -d 2021.07.26 -sh 14 -sub_tag SpringSBE > $rundir/SpringSBE.log
-python $rundir/tracker.py -gtx cas6_v00_uu0mb -exp sect_AImid -3d True -dtt 2 -clb True -d 2021.08.01 -sh 14 -sub_tag NeapSBF > $rundir/NeapSBF.log
-python $rundir/tracker.py -gtx cas6_v00_uu0mb -exp sect_AImid -3d True -dtt 2 -clb True -d 2021.08.02 -sh 8 -sub_tag NeapSBE > $rundir/NeapSBE.log
+python $rundir/tracker.py -gtx cas6_v00_uu0mb -exp sect_AImid -3d True -dtt 2 -clb True -d 2021.07.25 -sh 20 -sub_tag SpringSBF > $rundir/SpringSBF.log &
+python $rundir/tracker.py -gtx cas6_v00_uu0mb -exp sect_AImid -3d True -dtt 2 -clb True -d 2021.07.26 -sh 14 -sub_tag SpringSBE > $rundir/SpringSBE.log &
+python $rundir/tracker.py -gtx cas6_v00_uu0mb -exp sect_AImid -3d True -dtt 2 -clb True -d 2021.08.01 -sh 14 -sub_tag NeapSBF > $rundir/NeapSBF.log &
+python $rundir/tracker.py -gtx cas6_v00_uu0mb -exp sect_AImid -3d True -dtt 2 -clb True -d 2021.08.02 -sh 8 -sub_tag NeapSBE > $rundir/NeapSBE.log &
+
+wait
+echo "All runs complete"
 
