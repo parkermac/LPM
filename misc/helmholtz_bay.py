c4903766bda9834435e2f77d3991adc93c82aa86
Parker MacCready <p.maccready@gmail.com>
2024-03-19 11:10:45 -0700

    Update helmholtz_bay.py

--- misc/helmholtz_bay.py
+++ misc/helmholtz_bay.py
@@ -6,7 +6,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from lo_tools import plotting_functions as pfun
 
-r_om0 = .1 # R/omega0
+r_om0 = .4 # R/omega0
 
 omax = 3
 om_om0 = np.linspace(0,omax,500)# omega/omega0
@@ -14,7 +14,7 @@ om_om0 = np.linspace(0,omax,500)# omega/omega0
 phi = np.arctan2(r_om0*om_om0, 1 - om_om0**2) # phase lag [radians]
 # phi = np.arctan(r_om0*om_om0/(1 - om_om0**2)) # phase lag [radians]
 
-a_a0 = 1 / np.sqrt((1-om_om0**2)**2 + (r_om0*om_om0**2))
+a_a0 = 1 / np.sqrt((1-om_om0**2)**2 + (r_om0*om_om0)**2)
 
 pfun.start_plot()
 plt.close('all')
@@ -31,6 +31,6 @@ ax.set_title(r'Helmholtz Bay: $R/\omega_{0}=%0.1f$' % (r_om0))
 ax = fig.add_subplot(212)
 ax.plot(om_om0, a_a0)
 ax.set_xlabel(r'$\omega/\omega_{0}$')
-ax.set_ylim(0,4)
+ax.set_ylim(0,3)
 ax.grid(True)
 ax.set_xlim(0,omax)
