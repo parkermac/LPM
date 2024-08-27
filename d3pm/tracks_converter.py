fdaf88086f2630877461b1329c9ec539e17d836c
Parker MacCready <p.maccready@gmail.com>
2024-08-27 15:07:53 -0700

    Update tracks_converter.py

--- d3pm/tracks_converter.py
+++ d3pm/tracks_converter.py
@@ -33,34 +33,6 @@ dt_list = []
 for h in Time:
     dt_list.append(dt0 + timedelta(days=h/24))
 
-if False:
-    # Try removing out-of-bounds points
-
-    # RESULT: this worked, but it threw so many errors that it degraded performance.
-    # The problem was reading nans.
-
-    # make a mask that is False from the time a particle first leaves the domain
-    # and onwards
-    fng = dir0 / gtagex / exp / 'grid.nc'
-    dsg = xr.open_dataset(fng)
-
-    AA = [dsg.lon_rho.values[0,0], dsg.lon_rho.values[0,-1],
-            dsg.lat_rho.values[0,0], dsg.lat_rho.values[-1,0]]
-    ib_mask = np.ones(lon.shape, dtype=bool)
-    ib_mask[lon < AA[0]] = False
-    ib_mask[lon > AA[1]] = False
-    ib_mask[lat < AA[2]] = False
-    ib_mask[lat > AA[3]] = False
-    NTS, NPS = lon.shape
-    for pp in range(NPS):
-        tt = np.argwhere(ib_mask[:,pp]==False)
-        if len(tt) > 0:
-            ib_mask[tt[0][0]:, pp] = False
-
-    # and apply the mask to lon and lat
-    lon[~ib_mask] = np.nan
-    lat[~ib_mask] = np.nan
-
 # Create and save jsons.
 xy = []
 for pp in range(NP):
